ac5e63c62149b8de4aa9e618f8fb660dd25ab272
RAJerez <rodrigoagustin23@gmail.com>
2025-03-24 13:07:13 -0300

    mongo primeras implementaciones

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Iniciar el server: uvicorn app.main:app --reload o fastapi dev
 Documentacion con Swagger: http://127.0.0.1:8000/docs
 
 Documentacion con Redocli: http://127.0.0.1:8000/redoc
+
+conn_str: localhost:27017
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.routers import products, users, basic_auth_users, jwt_auth_users
+from app.routers import products, users, users_db, basic_auth_users, jwt_auth_users
 
 
 app = FastAPI()
@@ -8,6 +8,7 @@ app = FastAPI()
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(users_db.router)
 app.include_router(basic_auth_users.router)
 app.include_router(jwt_auth_users.router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
